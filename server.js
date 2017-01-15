ffaba4e734e94adc2eeff2659ce7de214a44799c
Srinithya-vutukuru <srinithya.vutukuru@gmail.com>
2017-01-15 21:38:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.get('/test-db',function(req,res){
         if(err){
             res.status500.send(err.toString());
         }else{
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });
